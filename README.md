122301f4e58b0cfd95afde056a66b24a88942dc7
Giorgos Margaritis <gmargari@cs.uoi.gr>
2017-04-02 22:00:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# mp3redspider
+
+Scrapy spider to search on mp3red.me and extract song titles and urls
+
+## Install
+
+```
+sudo pip install scrapy selenium twisted pyopenssl
+sudo apt-get install phantomjs
+```
+
+## Run
+
+From project's top level directory:
+```
+scrapy crawl mp3red --nolog
+```
---
+++ mp3red/spiders/mp3red_spider.py
@@ -0,0 +1,63 @@
+import scrapy
+from selenium import webdriver
+
+######################### sudo pip install --upgrade scrapy selenium twisted pyopenssl
+######################### sudo apt-get install phantomjs
+
+class Song(scrapy.Item):
+    title = scrapy.Field()
+    url = scrapy.Field()
+
+#===============================================================================
+#
+#===============================================================================
+class Mp3RedSpider(scrapy.Spider):
+    name = 'mp3red'  # spider name
+
+    max_pages = 3
+    cur_page = 0
+
+    #===========================================================================
+    # start_requests ()
+    #===========================================================================
+    def start_requests(self):
+        # Initialized our headless browser
+        self.browser = webdriver.PhantomJS()
+
+        self.browser.get('http://mp3red.me')
+
+        search_box = self.browser.find_element_by_id("search_str")
+        search_box.send_keys('Iron Maiden')
+
+        button = self.browser.find_element_by_xpath('//input[@class="button"]')
+        button.click()
+
+        current_url = self.browser.current_url
+        yield scrapy.Request(current_url, callback=self.parse_song_page)
+
+    #===========================================================================
+    # parse_song_page ()
+    #===========================================================================
+    def parse_song_page(self, response):
+
+        print('%s' % response.url)
+        self.cur_page += 1
+        if (self.cur_page > self.max_pages):
+            return
+
+        self.browser.get(response.url)
+
+        song_divs = self.browser.find_elements_by_xpath('//div[@class="player"]')
+        for song_div in song_divs:
+            song_title = song_div.get_attribute('data-title')
+            song_url = song_div.get_attribute('data-mp3url')
+
+            item = Song()
+            item['title'] = song_title
+            item['url'] = song_url
+            yield item
+
+            print("################## %s : %s " % (song_title, song_url))
+
+        next_page_url = self.browser.find_element_by_xpath('//a[contains(text(),"Next page")]').get_attribute('href')
+        yield scrapy.Request(next_page_url, callback=self.parse_song_page)
